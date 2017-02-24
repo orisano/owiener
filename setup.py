b1159bfd0c293a435fe1bdc205a246eac0b8af8a
Nao Yonashiro <owan.orisano@gmail.com>
2017-02-24 10:44:09 +0900

    initial commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name="wiener",
+    version="0.0.1",
+    author="orisano",
+    author_email="owan.orisano@gmail.com",
+    description="A Python3 implementation of the Wiener attack on RSA",
+    license="MIT",
+    url="https://github.com/orisano/wiener",
+    packages=["wiener"],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Security :: Cryptography",
+    ],
+)
