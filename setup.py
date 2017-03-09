a8d44d3416cd3a9db2aea5f43ebbe4c3fc9c886b
Nao Yonashiro <owan.orisano@gmail.com>
2017-03-09 11:57:48 +0800

    release 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="owiener",
-    version="0.0.1",
+    version="1.0.0",
     author="orisano",
     author_email="owan.orisano@gmail.com",
     description="A Python3 implementation of the Wiener attack on RSA",
@@ -10,7 +10,7 @@ setup(
     url="https://github.com/orisano/owiener",
     py_modules=["owiener"],
     classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
+        "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
