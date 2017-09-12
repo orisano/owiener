551464a4f8e804438d362776c5ce927b795193fd
Nao Yonashiro <owan.orisano@gmail.com>
2017-09-13 03:49:04 +0900

    feat: add long_description

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
+import os.path
+
 from setuptools import setup
+
+readme_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "README.md")
+try:
+    from m2r import parse_from_file
+    readme = parse_from_file(readme_file)
+except ImportError:
+    with open(readme_file) as f:
+        readme = f.read()
 
 setup(
     name="owiener",
-    version="1.0.0",
+    version="1.0.1",
     author="orisano",
     author_email="owan.orisano@gmail.com",
     description="A Python3 implementation of the Wiener attack on RSA",
+    long_description=readme,
     license="MIT",
     url="https://github.com/orisano/owiener",
     py_modules=["owiener"],
