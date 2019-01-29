b118423da385c35cd6d158fdf23b021e64901fe6
Nao YONASHIRO <yonashiro@r.recruit.co.jp>
2019-01-29 23:38:43 +0900

    chore: happy new year

--- owiener.py
+++ owiener.py
@@ -1,6 +1,6 @@
 """
 The MIT License (MIT)
-Copyright (c) 2017 orisano
+Copyright (c) 2019 orisano
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
