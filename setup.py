cbcf237d5ff20697ca290452ca0b8ed6ed40683f
Nao Yonashiro <owan.orisano@gmail.com>
2020-10-19 20:38:21 +0900

    chore: bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ URL = "https://github.com:/orisano/owiener"
 EMAIL = "owan.orisano@gmail.com"
 AUTHOR = "Nao Yonashiro"
 REQUIRES_PYTHON = ">=3.5.0"
-VERSION = "1.0.5"
+VERSION = "1.0.6"
 
 here = os.path.abspath(os.path.dirname(__file__))
 with io.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
